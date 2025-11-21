587d506f04138e505e5f286c10db35034029aaf3
Boris Mirsky <boris.y.mirsky@gmail.com>
2025-11-21 12:30:56 +0300

    first commit with backend

---
+++ backend/Chat/Chat/Hubs/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Chat.Models;
+
+namespace Chat.Hubs
+{
+
+
+    public interface IChatClient
+    {
+        public Task ReceiveMessage(string userName, string message);
+    }
+    public class ChatHub : Hub<IChatClient>
+    {
+        public async Task JoinChat(UserConnection connection)
+        {
+            // Одна группа это чат. Тут добавление юзера в чат
+            await Groups.AddToGroupAsync(
+                Context.ConnectionId,           // from Hub
+                connection.ChatRoom);           // from client
+
+            // оповещение что новый юзер в чате
+            await Clients.
+                Group(connection.ChatRoom).
+                ReceiveMessage("Admin", $"{connection.UserName} вошёл в чат");
+
+        }
+    }
+}
